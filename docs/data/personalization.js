c83ade39ec6705aea8903af3190567172ded4608
sakuramochi <sakuramochi.mochi.0@gmail.com>
2020-07-16 09:05:42 +0900

    Move files to docs/.

---
+++ docs/data/personalization.js
@@ -0,0 +1,28 @@
+window.YTD.personalization.part0 = [ {
+  "p13nData" : {
+    "demographics" : {
+      "languages" : [ {
+        "language" : "Japanese",
+        "isDisabled" : true
+      } ],
+      "genderInfo" : {
+        "gender" : "female"
+      }
+    },
+    "interests" : {
+      "interests" : [ ],
+      "partnerInterests" : [ ],
+      "audienceAndAdvertisers" : {
+        "numAudiences" : "0",
+        "advertisers" : [ ],
+        "lookalikeAdvertisers" : [ ],
+      },
+	"shows" : [ ],
+    },
+    "locationHistory" : [ ],
+    "inferredAgeInfo" : {
+      "age" : [ "13-54" ],
+      "birthDate" : ""
+    }
+  }
+} ]
